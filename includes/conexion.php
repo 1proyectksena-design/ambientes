e56162948694c379968d600244046aabb3973783
sena <1proyectksena@gmail.com>
2026-02-04 15:25:48 -0500

    cambios

---
+++ includes/conexion.php
@@ -0,0 +1,7 @@
+<?php
+$conexion = mysqli_connect("localhost","root","","ambientes");
+
+if(!$conexion){
+    die("Error de conexión");
+}
+?>
---
+++ subdireccion/consultar.php
@@ -0,0 +1,44 @@
+<?php
+include("../includes/conexion.php");
+
+$sql = "SELECT 
+            a.nombre_ambiente,
+            i.nombre_completo,
+            au.fecha,
+            au.hora_inicio,
+            au.hora_fin,
+            am.estado,
+            au.rol_autorizado
+        FROM autorizaciones_ambientes au
+        JOIN ambientes am ON au.id_ambiente = am.id_ambiente
+        JOIN instructores i ON au.id_instructor = i.id_instructor
+        JOIN ambientes a ON au.id_ambiente = a.id_ambiente";
+
+$resultado = mysqli_query($conexion, $sql);
+?>
+
+<h2>Consulta de Autorizaciones</h2>
+
+<table border="1">
+<tr>
+    <th>Ambiente</th>
+    <th>Instructor</th>
+    <th>Fecha</th>
+    <th>Hora inicio</th>
+    <th>Hora fin</th>
+    <th>Estado</th>
+    <th>Autorizado por</th>
+</tr>
+
+<?php while($row = mysqli_fetch_assoc($resultado)){ ?>
+<tr>
+    <td><?= $row['nombre_ambiente'] ?></td>
+    <td><?= $row['nombre_completo'] ?></td>
+    <td><?= $row['fecha'] ?></td>
+    <td><?= $row['hora_inicio'] ?></td>
+    <td><?= $row['hora_fin'] ?></td>
+    <td><?= $row['estado'] ?></td>
+    <td><?= $row['rol_autorizado'] ?></td>
+</tr>
+<?php } ?>
+</table>
